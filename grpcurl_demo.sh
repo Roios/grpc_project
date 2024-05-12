01d58c3d89af4b4e86bd7fbd2f43a5f505c3cf46
Roios <pedro.roios@gmail.com>
2024-05-12 16:30:54 +0200

    add dummy requests for test

--- grpcurl_demo.sh
+++ grpcurl_demo.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
-output1=$(grpcurl -plaintext localhost:8888 list)
 echo "Output of 'grpcurl -plaintext localhost:8888 list' :"
+output1=$(grpcurl -plaintext localhost:8888 list)
 echo "$output1"
 echo
 
-output2=$(grpcurl -plaintext localhost:8888 list Orders)
 echo "Output of 'grpcurl -plaintext localhost:8888 list Orders' :"
+output2=$(grpcurl -plaintext localhost:8888 list Orders)
 echo "$output2"
 echo
 
-output3=$(grpcurl -plaintext -proto orders.proto localhost:8888 describe Orders.RegisterOrder)
 echo "Output of 'grpcurl -plaintext -proto orders.proto localhost:8888 describe Orders.RegisterOrder' :"
+output3=$(grpcurl -plaintext -proto orders.proto localhost:8888 describe Orders.RegisterOrder)
 echo "$output3"
 echo
 
-output4=$(grpcurl -plaintext localhost:8888 describe .StartResponse)
 echo "Output of 'grpcurl -plaintext localhost:8888 describe .StartResponse' :"
+output4=$(grpcurl -plaintext localhost:8888 describe .StartResponse)
 echo "$output4"
+echo
+
+echo "Output of 'grpcurl -plaintext -d @ localhost:8888 Orders.RegisterOrder < dummy_request.json' :"
+output5=$(grpcurl -plaintext -d @ localhost:8888 Orders.RegisterOrder < dummy_request.json)
+echo "$output5"
+echo
+
+echo "Output (should fail) of 'grpcurl -plaintext -d @ localhost:8888 Orders.RegisterOrder < dummy_request_2.json' :"
+output6=$(grpcurl -plaintext -d @ localhost:8888 Orders.RegisterOrder < dummy_request_2.json)
+echo "$output6"
 echo
